2c35fbd1d4cab40372b5f9e7a1a9ab8b9b439f0d
Reinhard Pointner <reinhard.pointner@gmail.com>
2018-06-05 14:20:40 +0700

    Refactor common media characteristics (e.g. to use ffprobe instead of libmediainfo internally for various use cases)

---
+++ source/net/filebot/media/MediaCharacteristicsParser.java
@@ -0,0 +1,26 @@
+package net.filebot.media;
+
+import java.io.File;
+
+import net.filebot.mediainfo.MediaInfo;
+
+public enum MediaCharacteristicsParser {
+
+	libmediainfo, ffprobe;
+
+	public static MediaCharacteristicsParser getDefault() {
+		return SystemProperty.of("net.filebot.media.parser", MediaCharacteristicsParser::valueOf, libmediainfo).get();
+	}
+
+	public static MediaCharacteristics open(File f) throws Exception {
+		switch (getDefault()) {
+		case libmediainfo:
+			return new MediaInfo().open(f);
+		case ffprobe:
+			return new FFProbe().open(f);
+		}
+
+		throw new IllegalStateException();
+	}
+
+}
